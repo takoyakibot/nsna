56dac11fa26cbc4f4baefbda35232e7889b44c07
Raging Alpaca <lamune.cider@gmail.com>
2018-05-16 09:09:16 +0900

    “hoge”

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -70,12 +70,13 @@ class CharacterController extends Controller
     {
         $character = null_escape(Character::where('id_rand', $id_rand)->first(), new Character);
 
+        $imgpath = null;
+        $savepath = null;
+
         // 画像を保存
         // 画像が選択されていれば保存する
         foreach ($_FILES as $file)
         {
-            $imgpath = null;
-
             // 各種情報の取得
             $tmp_name = $file['tmp_name'];
             $size = $file['size'];
